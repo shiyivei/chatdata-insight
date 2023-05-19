ebb013b00331b56aae868cd4c5615ebbfbf8b9da
todaylab <jinrishouji@sina.com>
2023-05-20 06:52:53 +0800

    clean frontend

--- frontend/polarapp/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return (
-      <div className="bg-black text-white flex justify-center items-center h-24">
-        <div>A PolarDASH stack based application for cars</div>
-        </div>
-    )
-  }
-  export default Footer
