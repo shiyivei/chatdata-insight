e4e6ae8b519fc477c9fe748860d821e729d5eb49
todaylab <jinrishouji@sina.com>
2023-06-11 09:12:58 +0800

    add ErrorQuestionRecord class

--- backend/services/helpers/question_db.py
+++ backend/services/helpers/question_db.py
@@ -6,17 +6,23 @@ from pydantic import BaseModel
 # Data Pydantic model
 class Data(BaseModel):
     question: str
+    answer: str
     error: Optional[str] = None
 
-# insert data
-async def insert_error_data(question: str, answer: str, error: str):
-    collection = app.mongodb["chatadata-insight-collection"]  
-    data = Data(question=question, answer=answer, error=error)
-    data_dict = data.dict()
-    result = await collection.insert_one(data_dict)
-    if result:
-        return {"status": "data inserted successfully"}
-    else:
-        raise HTTPException(status_code=400, detail="Insertion failed")
+class ErrorQuestionRecord():
+    def __init__(self):
+        collection = app.mongodb["chatadata-insight-collection"]
+        collection.createIndex( {'question':1} )
 
+    @classmethod
+    # insert data
+    async def insert_error_data(cls, question: str, answer: str, error: str):
+        collection = app.mongodb["chatadata-insight-collection"]  
+        data = Data(question=question, answer=answer, error=error)
+        data_dict = data.dict()
+        result = await collection.insert_one(data_dict)
+        if result:
+            return {"status": "data inserted successfully"}
+        else:
+            raise HTTPException(status_code=400, detail="Insertion failed")
 
--- backend/services/integration/integration.py
+++ backend/services/integration/integration.py
@@ -20,6 +20,7 @@ from core.config import Config
 from services.binance import binance_api
 from services.news import news_api
 from services.onchain_info.airstack_and_quicknode import onchain_info_agent
+from services.helpers.question_db import ErrorQuestionRecord
 
 os.environ["OPENAI_API_KEY"] = Config.OPENAI_API_KEY
 
@@ -127,7 +128,7 @@ def get_binance_prams(x):
 
 def search_on_internet(question: str) -> str:
 
-    insert_error_data(question, "", "can not find answer")
+    ErrorQuestionRecord.insert_error_data(question, "", "can not find answer")
 
     os.environ["GOOGLE_CSE_ID"] = "6170c8edfbf634caf"
     os.environ["GOOGLE_API_KEY"] = "AIzaSyDnFWoQElznz9N5frGoVsOuNP55xBBV6zM"
